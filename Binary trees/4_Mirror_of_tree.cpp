e8692ee9be5b6610d58c752392de7bf4799cca5a
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-06-05 16:31:04 +0530

    Update 4_Mirror_of_tree.cpp

--- Binary trees/4_Mirror_of_tree.cpp
+++ Binary trees/4_Mirror_of_tree.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* root) {
+        // code here
+        
+        // LOGIC :-
+        /* Mirror ka matlab har ek node ko swap kar do
+        Isme hame root ko kuch karne ki zrurt nhi hai wo waisa ka waisa hi rahega
+        left subtree aur right subtree ke recursion apne aap sab kuch kar dega
+        bacha root->left, aur root->right node to ye dono ko ham last me swap kar denge
+        */
+        if(root == NULL) return;
+        mirror(root->left);
+        mirror(root->right);
+        swap(root->left, root->right);
+    }
+};
