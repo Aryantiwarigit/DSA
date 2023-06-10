7f71dc69d8564bc5d5a97d8ff53cd2b1769f968f
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-06-10 13:20:29 +0530

    Update 3_Check_balance_tree.cpp

--- Binary trees/3_Check_balance_tree.cpp
+++ Binary trees/3_Check_balance_tree.cpp
@@ -1,1 +1,19 @@
+// Time complexity -> O(N^2)
+class Solution {
+    int height(TreeNode* root){
+        if(root ==NULL)return 0;
+        int l = height(root->left);
+        int r = height(root->right);
+        return max(l,r)+1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        bool left = isBalanced(root->left);
+        bool right = isBalanced(root->right);
+        bool third = abs(height(root->left)-height(root->right))<=1;
 
+        if(left && right && third) return true;
+        return false;
+    }
+};
