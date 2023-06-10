4b5431e6f96fe3c620cc6cce713a7f7e4fcea2e8
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-06-10 12:36:10 +0530

    Update 1_count_leaf_nodes.cpp

--- Binary trees/1_count_leaf_nodes.cpp
+++ Binary trees/1_count_leaf_nodes.cpp
@@ -1,1 +1,25 @@
+// CONFUSION :- isme agar usi countLeaves waale function me solve kar rhe hain to solve nhi ho rha hai.
+// Note :- we can solve this by any type of traversal like inorder, preorder and postorder 
 
+void postorder(Node* root, int& count){
+      if(root == NULL)return;
+      
+    
+      postorder(root->left,count);
+      postorder(root->right,count);
+      if(root->left==NULL && root->right==NULL){
+          count++;
+      }
+
+}
+int countLeaves(Node* root)
+{
+  // Your code here
+  int count = 0;
+   
+   postorder(root,count);
+  
+  return count;
+  
+  
+}
