f38b7e49f0f20e82a3e4c4801720c3416b9e5b12
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-07-23 19:50:36 +0530

    Update 7_replace_element_by_its_rank.cpp

--- Array Basic Qs/7_replace_element_by_its_rank.cpp
+++ Array Basic Qs/7_replace_element_by_its_rank.cpp
@@ -1,1 +1,23 @@
+/*
+Replace elements by its rank in the array. EXAMPLE -->
+arr = [20, 15, 26, 2, 98, 6]
+Output:
+4, 3, 5, 1, 6, 2
+Explanation:
+After sorting, array becomes {2,6,15,20,26,98}
+*/
 
+// NOTE :- This solution is throwing TLE in gfg so we need to optimize this.
+
+vector<int> replaceWithRank(vector<int> &arr, int N){
+    map<int,int> mp;
+    for(auto x : arr){
+        mp[x]++;
+    }
+    
+    for(int i=0; i<N; i++){
+        int ind = distance(mp.begin(), mp.find(arr[i]));
+        arr[i] = ind+1;
+    }
+    return arr;
+}
