e024ff40f64bb1163c07ea180804b15cd9629086
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-07-23 18:03:49 +0530

    Update 4_Remove_duplicate_elements.cpp

--- Array Basic Qs/4_Remove_duplicate_elements.cpp
+++ Array Basic Qs/4_Remove_duplicate_elements.cpp
@@ -1,1 +1,13 @@
+// IMPORTANT QS --> This require a simple algorithm (watch video)
 
+int remove_duplicate(int a[],int n){
+    // code here
+    int i=0;
+    for(int j=0; j<n; j++){
+        if(i==0 || a[i-1]!=a[j]){
+            a[i] = a[j];
+            i++;
+        }
+    }
+    return i;
+}
