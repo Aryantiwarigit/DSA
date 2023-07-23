dd8492cb73a11306fea7c2fad082bc353420ccf8
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-07-23 19:34:15 +0530

    Update 5_Minimum_absolute_difference.cpp

--- Array Basic Qs/5_Minimum_absolute_difference.cpp
+++ Array Basic Qs/5_Minimum_absolute_difference.cpp
@@ -1,3 +1,12 @@
+// Input: nums = [2, 4, 5, 9, 7]
+// Output: 1
+// Explanation: Difference between 5 and 4 is 1.
+
+/*
+Input: nums = [3, 10, 8, 6]
+Output: 2
+Explanation: Difference between 8 and 6 is 2.
+*/
 int minimum_difference(vector<int>nums){
     // Code here
     sort(nums.begin(), nums.end());
