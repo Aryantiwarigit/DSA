1b389bf490df351406979548e933a4d38a9ca0db
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-07-23 19:32:53 +0530

    Update 5_Minimum_absolute_difference.cpp

--- Array Basic Qs/5_Minimum_absolute_difference.cpp
+++ Array Basic Qs/5_Minimum_absolute_difference.cpp
@@ -1,1 +1,10 @@
-
+int minimum_difference(vector<int>nums){
+    // Code here
+    sort(nums.begin(), nums.end());
+    int min = INT_MAX;
+    for(int i=0; i<nums.size()-1; i++){
+        int diff = abs(nums[i]-nums[i+1]);
+        if(diff < min) min = diff;
+    }
+    return min;
+}  
