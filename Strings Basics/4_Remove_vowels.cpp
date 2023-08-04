902a9844506db494e57a9c9c9572c6b06b91cd7f
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-08-04 11:37:36 +0530

    Update 4_Remove_vowels.cpp

--- Strings Basics/4_Remove_vowels.cpp
+++ Strings Basics/4_Remove_vowels.cpp
@@ -1,1 +1,12 @@
+	{ 
+	    int n = s.size();
+	    int j = 0;
 
+	    for(int i = 0; i < n; i++)
+	    {
+	    	if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u')
+	    		s[j++] = s[i];
+	    }
+
+	    return s.substr(0, j);
+	} 
