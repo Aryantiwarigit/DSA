64be88bc9f0ad7c50e1981d3d4eb276ce7ffbb24
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-08-05 19:36:02 +0530

    Update 8_Replace_all_substr.cpp

--- Strings Basics/8_Replace_all_substr.cpp
+++ Strings Basics/8_Replace_all_substr.cpp
@@ -1,1 +1,15 @@
+// GFG QUESTION link --> https://practice.geeksforgeeks.org/problems/replace-a-word5553/1
 
+public:
+  string replaceAll(string str, string oldW, string newW) {
+      // code here
+      int pos = str.find(oldW); //pos variable finds all the indexes of the string that we've to replace
+      while(pos = str.find(oldW)!=string::npos){ //npos means no position is found
+          // simply replace 
+          str.replace(pos, oldW.size(), newW);
+          
+          // increment the position
+          pos = str.find(oldW) + newW.size();
+      }
+      return str;
+  }
