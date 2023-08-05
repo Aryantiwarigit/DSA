e57787d04d1da3d1df1870651ea082f252741fa0
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-08-05 10:51:38 +0530

    Update 5_Remove_spaces.cpp

--- Strings Basics/5_Remove_spaces.cpp
+++ Strings Basics/5_Remove_spaces.cpp
@@ -1,1 +1,15 @@
-
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+        //code here.
+        int pos=0;
+        for(int i=0; i<s.size(); i++){
+            if(!(s[i]==' ')){
+                s[pos] = s[i]; pos++;
+            }
+        }
+        return s.substr(0,pos);
+    }
+};
