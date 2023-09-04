54f8ddbde3bb8a973ee341dfb8d9107b239b313d
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-09-04 13:09:09 +0530

    Update 1_ASCII_of_character.cpp

--- Strings Basics/1_ASCII_of_character.cpp
+++ Strings Basics/1_ASCII_of_character.cpp
@@ -6,3 +6,21 @@ int main(){
     cout<<"ASCII value of "<<ch<<" is "<<int(ch)<<endl;
     return 0;
 }
+
+/* String of Digits to int value :- "12345"(string) to 12345(int)
+To do this there is a function string to int known as stoi(string);
+*/
+int main(){
+    string s = "123";
+    int a = stoi(s);
+    cout<<a+2;
+    return 0;
+}
+
+/* to convert any number character to its int value. Eg- '3' to 3
+simply character 0 minus kardo us character me se like 
+(any chracter) - '0' = int value of that character;
+*/
+    char a = '9';
+    int b = a-'0';
+    cout<<b+1; //-> this will give 10.
