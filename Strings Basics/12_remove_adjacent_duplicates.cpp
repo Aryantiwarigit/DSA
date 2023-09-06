f7ba5f919fad2fd1f1b98c56073be22c256f1d3a
Aryantiwarigit <134907789+Aryantiwarigit@users.noreply.github.com>
2023-09-06 20:56:26 +0530

    Update 12_remove_adjacent_duplicates.cpp

--- Strings Basics/12_remove_adjacent_duplicates.cpp
+++ Strings Basics/12_remove_adjacent_duplicates.cpp
@@ -1,1 +1,21 @@
+/* Input: s = "abbaca" Output: "ca"
+   Input: s = "azxxzy" Output: "ay"
+*/
 
+string ans;
+stack<char> st;
+for(int i=0; i<s.size(); i++){
+    if(st.empty() || s[i]!=st.top()){
+        st.push(s[i]);
+    }
+    else{
+        st.pop();
+    }
+}
+while(!st.empty()){
+    char c = st.top();
+    ans.push_back(c);
+    st.pop();
+}
+reverse(ans.begin(), ans.end());
+return ans;
